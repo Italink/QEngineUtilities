5a357cd4818d3e663e21bcd55c8c01a25198f6b2
italink <657959053@qq.com>
2024-11-23 03:38:28 +0800

    [commit] vulkan内存分配器不同版本支持

--- Source/Core/Source/Private/Render/RHI/Vulkan/vk_mem_alloc.h
+++ Source/Core/Source/Private/Render/RHI/Vulkan/vk_mem_alloc.h
@@ -1,7 +1,7 @@
-#if QT_VERSION >= QT_VERSION_CHECK(6, 7, 0)
+#if QT_VERSION >= QT_VERSION_CHECK(6, 8, 0)
+#include "vk_mem_alloc_6_8_0.h"
+#elif QT_VERSION >= QT_VERSION_CHECK(6, 7, 0)
 #include "vk_mem_alloc_6_7_0.h"
-#elif QT_VERSION >= QT_VERSION_CHECK(6, 6, 0)
-#include "vk_mem_alloc_6_6_0.h"
 #else
 #error This Qt Version Not Supported
 #endif
